9589da4ab1649d5ee1bb0d086d21fd4ea52c52b0
SaKura丶 <508371431@qq.com>
2017-11-14 13:56:58 +0800

    update

--- blogproject/blogproject/settings.py
+++ blogproject/blogproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '68b!(iq91=o^oq1@c@s^$vjdhqym3a#i!bzynw=$$kxm)uahgb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
